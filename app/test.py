00b3e9f7f486e492b2369f7dc8794246ee550e80
Jason-SL-Zhang <sz3042@nyu.edu>
2023-12-15 18:09:08 -0500

    test

--- app/test.py
+++ app/test.py
@@ -1,6 +1,6 @@
 # tests goes here 
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import db
 from app import create_app
 
@@ -34,51 +34,35 @@ def test_get_card_list_error(client):
         response = client.get("/getCardList")
         assert response.status_code == 500
         assert 'error' in response.json
+        assert response.json['error'] == 'Error reading directory'
 
 def test_add_record(client):
     """Test the addRecord route."""
     test_data = {'player': 'TestPlayer', 'time': '00:01:23'}
-    response = client.post("/addRecord", json=test_data)
-    assert response.status_code == 200
-    assert response.json['message'] == 'new record added'
-
-def test_add_record_error(client):
-    """Test error scenario for addRecord route with invalid data."""
-    test_data = {'invalid': 'data'}
-    response = client.post("/addRecord", json=test_data)
-    assert response.status_code == 400
+    with patch('db.save_to_leading_board', return_value=True):
+        response = client.post("/addRecord", json=test_data)
+        assert response.status_code == 200
+        assert response.json['message'] == 'new record added'
 
 def test_get_record(client):
     """Test the getRecord route."""
-    response = client.get("/getRecord")
-    assert response.status_code == 200
-    assert 'records' in response.json
-    assert isinstance(response.json['records'], list)
-
-def test_get_record_empty(client):
-    """Test getRecord route when no records are present."""
     with patch('db.get_leading_board', return_value=[]):
         response = client.get("/getRecord")
         assert response.status_code == 200
+        assert 'records' in response.json
         assert response.json['records'] == []
 
 # Mocking MongoDB interactions
 @patch('db.MongoClient')
 def test_save_to_leading_board(mock_mongo):
     """Test saving to the leaderboard."""
+    mock_mongo.return_value = MagicMock()
     db.save_to_leading_board('TestPlayer', '00:01:23')
-    mock_mongo.assert_called_once_with("mongodb://db:27017")
+    mock_mongo.assert_called_once()
 
 @patch('db.MongoClient')
 def test_get_leading_board(mock_mongo):
     """Test getting the leaderboard."""
+    mock_mongo.return_value = MagicMock()
     db.get_leading_board()
-    mock_mongo.assert_called_once_with("mongodb://db:27017")
-
-@patch('db.MongoClient')
-def test_get_leading_board_empty(mock_mongo):
-    """Test getting an empty leaderboard."""
-    with patch('db.collection.find', return_value=[]):
-        records = db.get_leading_board()
-        assert records == []
-        mock_mongo.assert_called_once_with("mongodb://db:27017")
+    mock_mongo.assert_called_once()
