df9507583c51cc8f043127548c481fd8aeb3d181
Jason-SL-Zhang <sz3042@nyu.edu>
2023-12-15 17:37:58 -0500

    test

--- app/test.py
+++ app/test.py
@@ -1,5 +1,6 @@
-#tests goes here 
+# tests goes here 
 import pytest
+from unittest.mock import patch
 import db
 from app import create_app
 
@@ -11,10 +12,45 @@ def app():
 
 @pytest.fixture
 def client(app):
-    """create client"""
+    """Create client for testing."""
     return app.test_client()
 
+# Testing Flask routes
 def test_index(client):
-    """test index page"""
+    """Test index page."""
     response = client.get("/")
     assert response.status_code == 200
+
+def test_get_card_list(client):
+    """Test the getCardList route."""
+    response = client.get("/getCardList")
+    assert response.status_code == 200
+    assert 'cards' in response.json
+    assert isinstance(response.json['cards'], list)
+
+def test_add_record(client):
+    """Test the addRecord route."""
+    test_data = {'player': 'TestPlayer', 'time': '00:01:23'}
+    response = client.post("/addRecord", json=test_data)
+    assert response.status_code == 200
+    assert response.json['message'] == 'new record added'
+
+def test_get_record(client):
+    """Test the getRecord route."""
+    response = client.get("/getRecord")
+    assert response.status_code == 200
+    assert 'records' in response.json
+    assert isinstance(response.json['records'], list)
+
+# Mocking MongoDB interactions
+@patch('db.MongoClient')
+def test_save_to_leading_board(mock_mongo):
+    """Test saving to the leaderboard."""
+    db.save_to_leading_board('TestPlayer', '00:01:23')
+    mock_mongo.assert_called_once_with("mongodb://db:27017")
+
+@patch('db.MongoClient')
+def test_get_leading_board(mock_mongo):
+    """Test getting the leaderboard."""
+    db.get_leading_board()
+    mock_mongo.assert_called_once_with("mongodb://db:27017")
